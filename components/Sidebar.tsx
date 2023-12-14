f1bfca91a890907368f2742aef5390717cadb30e
PrAndrian <princydruh@gmail.com>
2023-12-14 13:31:40 +0100

    sidebar link added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,83 @@
+import { cn } from '@/lib/utils'
+import { Bot, Gamepad, LayoutDashboard, Settings } from 'lucide-react'
+import { Montserrat } from 'next/font/google'
 import Link from 'next/link'
 import React from 'react'
+
+const monserrat = Montserrat({
+    weight:'600',
+    subsets:['latin'],
+})
+
+const routes = [
+    {
+        label:"Dashboard",
+        icon: LayoutDashboard,
+        href:"/dashboard",
+        color:"text-sky-500"
+    },
+    {
+        label:"Ai correspondant",
+        icon: Bot,
+        href:"/ai_correspondant",
+        color:"text-purple-500"
+    },
+    {
+        label:"Word games",
+        icon: Gamepad,
+        href:"/word_game",
+        color:"text-orange-500"
+    },
+    {
+        label:"Setting",
+        icon: Settings,
+        href:"/setting",
+        color:"text-gray-500"
+    },
+]
 
 type Props = {}
 
 const Sidebar = (props: Props) => {
   return (
-    <div className='space-y-4 py-4 flex flex-col h-full w-full'>
-            <Link href="/dashboard" className='text-center text-3xl font-bold text-white space-x-5'>
-                Lingu&apos;AI
-            </Link>
+    <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
+        <div className='px-3 py-4 flex-1'>
+            <div className='text-center text-white mb-14'>
+                <Link href="/dashboard" className=''>
+                    <h1 className={cn("text-2xl font-bold",monserrat.className)}>Lingu&apos;AI</h1>  
+                </Link>
+            </div>
+
+            <div className='space-y-1'>
+                {routes.map((route)=>(
+                    <Link 
+                        key={route.href} 
+                        href={route.href}
+                        className='
+                            flex 
+                            group
+                            text-sm
+                            p-3
+                            w-full
+                            justify-start
+                            font-medium
+                            cursor-pointer
+                            hover:text-white
+                            hover:bg-white/10
+                            rounded-lg
+                            transition
+                        '
+                    >
+                        <div className="flex items-center flex-1">
+                            <route.icon className={cn("h-5 w-5 mr-3",route.color)}/>
+                            {route.label}    
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+
+            
     </div>
   )
 }
