842f6571a1b5edbe7c04e8dcb5ec11a37dd68cee
PrAndrian <princydruh@gmail.com>
2023-12-14 13:51:38 +0100

    mobile sidebar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,14 @@ import { Menu } from 'lucide-react'
 import React from 'react'
 import { Button } from './ui/button'
 import { UserButton } from '@clerk/nextjs'
+import MobileSidebar from './MobileSidebar'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
     <header className='flex items-center p-4'>
-        <Button variant="ghost" size="icon" className='md:hidden'>
-            <Menu/>
-        </Button>
+        <MobileSidebar/>
         <div className='flex w-full justify-end'>
             <UserButton afterSignOutUrl='/'/>
         </div>
