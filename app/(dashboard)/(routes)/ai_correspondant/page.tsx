f72e098e0286ce2d9f96393477fa43b342b668c2
PrAndrian <princydruh@gmail.com>
2023-12-15 14:44:05 +0100

    missed up

--- app/(dashboard)/(routes)/ai_correspondant/page.tsx
+++ app/(dashboard)/(routes)/ai_correspondant/page.tsx
@@ -36,64 +36,66 @@ const AiCorrespondant = (props: Props) => {
   }
 
   return (
-    <div>
-        <Heading
-          title='Ai correspondant'
-          description='Our most advenced conversation'
-          icon={MessageSquare}
-          iconColor='text-violet-500'
-          bgColor='bg-violet-500/10'
-        />
-        
-        <div className='flex flex-col space-y-2 px-4 lg:px-8'>
-
-          <div className="grow ">
-            Messages  
+    <>    
+      <div>
+          <Heading
+            title='Ai correspondant'
+            description='Our most advenced conversation'
+            icon={MessageSquare}
+            iconColor='text-violet-500'
+            bgColor='bg-violet-500/10'
+          />
+          
+          <div className='px-4 lg:px-8'>
+            <div className="">
+              <Form {...form}>
+                <form 
+                  onSubmit={form.handleSubmit(onSubmit)} 
+                  className="
+                    fixed bottom-10
+                    rounded-lg
+                    border
+                    w-full
+                    p-4
+                    px-3
+                    md:px-6
+                    focus-within:shadow-sm
+                    grid
+                    grid-cols-12
+                    gap-2
+                  "
+                >
+                  <FormField
+                    control={form.control}
+                    name="prompt"
+                    render={({ field }) => (
+                      <FormItem className="col-span-12 lg:col-span-10">
+                        <FormControl className=" m-0 p-0">
+                          <Input 
+                            className="
+                              border-0 outline-none 
+                              focus-visible:ring-0 
+                              focus-visible:ring-transparent
+                            "
+                            placeholder="Hello ! How to say ..." 
+                            disabled={isLoading} 
+                            {...field} 
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="col-span-12 lg:col-span-2">Send</Button>
+                </form>
+              </Form>
+            </div>
+            
+            <div className="">
+              azeae
+            </div>
           </div>
-
-          <div className="">
-            <Form {...form}>
-              <form 
-                onSubmit={form.handleSubmit(onSubmit)} 
-                className="
-                  rounded-lg
-                  border
-                  w-full
-                  p-4
-                  px-3
-                  md:px-6
-                  focus-within:shadow-sm
-                  grid
-                  grid-cols-12
-                  gap-2
-                "
-              >
-                <FormField
-                  control={form.control}
-                  name="prompt"
-                  render={({ field }) => (
-                    <FormItem className="col-span-12 lg:col-span-10">
-                      <FormControl className=" m-0 p-0">
-                        <Input 
-                          className="
-                            border-0 outline-none 
-                            focus-visible:ring-0 
-                            focus-visible:ring-transparent
-                          "
-                          placeholder="Helle ! How to say ..." 
-                          disabled={isLoading} 
-                          {...field} 
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="col-span-12 lg:col-span-2">Send</Button>
-              </form>
-            </Form>
-          </div>
-        </div>
-    </div>
+      </div>
+    </>
   )
 }
 
