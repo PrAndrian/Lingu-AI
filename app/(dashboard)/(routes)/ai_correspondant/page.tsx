c3c98d92ed6f5249c882e202c1cb6e60f9e298d8
PrAndrian <princydruh@gmail.com>
2023-12-15 13:01:29 +0100

    box message ui done

--- app/(dashboard)/(routes)/ai_correspondant/page.tsx
+++ app/(dashboard)/(routes)/ai_correspondant/page.tsx
@@ -1,10 +1,40 @@
-import Heading from '@/components/Heading'
+'use client'
+
+import * as z from "zod"
+import { useForm } from 'react-hook-form'
 import { MessageSquare } from 'lucide-react'
-import React from 'react'
+import Heading from '@/components/Heading'
+
+import {formSchema} from "./constants"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { 
+  Form,
+  FormControl, 
+  FormDescription, 
+  FormField, 
+  FormItem, 
+  FormMessage 
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 type Props = {}
 
 const AiCorrespondant = (props: Props) => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt:""
+    }
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit =  async (values : z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
     <div>
         <Heading
@@ -14,6 +44,55 @@ const AiCorrespondant = (props: Props) => {
           iconColor='text-violet-500'
           bgColor='bg-violet-500/10'
         />
+        
+        <div className='flex flex-col space-y-2 px-4 lg:px-8'>
+
+          <div className="grow ">
+            Messages  
+          </div>
+
+          <div className="">
+            <Form {...form}>
+              <form 
+                onSubmit={form.handleSubmit(onSubmit)} 
+                className="
+                  rounded-lg
+                  border
+                  w-full
+                  p-4
+                  px-3
+                  md:px-6
+                  focus-within:shadow-sm
+                  grid
+                  grid-cols-12
+                  gap-2
+                "
+              >
+                <FormField
+                  control={form.control}
+                  name="prompt"
+                  render={({ field }) => (
+                    <FormItem className="col-span-12 lg:col-span-10">
+                      <FormControl className=" m-0 p-0">
+                        <Input 
+                          className="
+                            border-0 outline-none 
+                            focus-visible:ring-0 
+                            focus-visible:ring-transparent
+                          "
+                          placeholder="Helle ! How to say ..." 
+                          disabled={isLoading} 
+                          {...field} 
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="col-span-12 lg:col-span-2">Send</Button>
+              </form>
+            </Form>
+          </div>
+        </div>
     </div>
   )
 }
--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,6 +1,14 @@
 import { cn } from '@/lib/utils'
 import { LucideIcon } from 'lucide-react'
 import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+  
 
 interface HeadingProps  {
     title:string,
@@ -19,16 +27,28 @@ const Heading = ({
 }: HeadingProps) => {
   return (
     <>
-        <div className='px-4 lg:px-8 flex items-center gap-x-3'>
-            <div className={cn('p-2 w-fit rounded-md',bgColor)}>
-                <Icon className={cn("w-10 h-10",iconColor)}/>
+        <div className='px-4 lg:px-8 flex items-center justify-between gap-x-3 mb-8'>
+            <div className='flex items-center gap-x-3'>
+                <div className={cn('p-2 w-fit rounded-md',bgColor)}>
+                    <Icon className={cn("w-10 h-10",iconColor)}/>
+                </div>
+                <div>
+                    <h2 className='text-xl lg:text-3xl font-bold'>{title}</h2>
+                    <p className='text-sm text-muted-foreground'>
+                        {description}
+                    </p>
+                </div>
             </div>
-            <div>
-                <h2 className='text-3xl font-bold'>{title}</h2>
-                <p className='text-sm text-muted-foreground'>
-                    {description}
-                </p>
-            </div>
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Language" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="system">Korean</SelectItem>
+                    <SelectItem value="light">French</SelectItem>
+                    <SelectItem value="dark">English</SelectItem>
+                </SelectContent>
+            </Select>
         </div>
     </>
   )
